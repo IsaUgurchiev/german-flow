73c5b90821e7ced4ddc12ba8d808113143cceb7c
i.ugurchiev <i.ugurchiev@criteo.com>
2025-12-23 21:06:47 +0100

    Fix prod url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://german-flow-backend.onrender.com/api', // Example production URL
+  apiBaseUrl: 'https://german-flow-y1ki.onrender.com',
   googleClientId: '367457967036-9s7q0n3jaf5j90e7l3rjsc6lom9msobm.apps.googleusercontent.com'
 };
 
